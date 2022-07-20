1531207420e6d87b624b03a8d1baa6dfc904ec5a
jesse <corbinjesse95@gmail.com>
2022-07-20 15:11:54 -0400

    link functions and numbers to buttons

--- script.js
+++ script.js
@@ -17,3 +17,36 @@ function divide(x, y) {
 function operate(operation, x, y) {
     return operation(x,y);
 }
+
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.math');
+const output = document.querySelector('.display');
+
+
+let currdis = 9;
+currdis +=10;
+output.textContent = currdis;
+//use to update the display while running functions?
+
+numbers.forEach((numButton) => {
+    numButton.addEventListener('click', () => {
+        output.textContent = numButton.id;
+       
+    });
+});
+
+let opfunc;
+//can turn this into a swtich statement
+operators.forEach((opButton) => {
+    opButton.addEventListener('click', () => {
+        if (opButton.id === 'add') {
+            opfunc = adds;
+        } else if (opButton.id === 'subtract') {
+            opfunc = subtracts;
+        } else if (opButon.id === 'multiply') {
+            opfunc = multiply;
+        } else if (opButton.id === 'divide') {
+            opfunc = divide;
+        } 
+    })
+})
