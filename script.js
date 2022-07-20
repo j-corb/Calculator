dfef413ae0eeae633620202e1a19f11ce9790a2e
jesse <corbinjesse95@gmail.com>
2022-07-20 08:20:44 -0400

    add operations functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function adds(x, y) {
+    return x+y;
+}
+
+function subtracts(x, y) {
+    return x-y;
+}
+
+function multiply(x, y) {
+    return x*y;
+}
+
+function divide(x, y) {
+    return x/y;
+}
+
+function operate(operation, x, y) {
+    return operation(x,y);
+}
