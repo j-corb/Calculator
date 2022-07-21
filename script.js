df6383c8865a3c9f218a576dd61542652ba8dc4a
jesse <corbinjesse95@gmail.com>
2022-07-21 12:07:38 -0400

    add way to store which buttons are pressed

--- script.js
+++ script.js
@@ -21,21 +21,30 @@ function operate(operation, x, y) {
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.math');
 const output = document.querySelector('.display');
+const buttons = document.querySelectorAll('.button');
 
-
+let workingVals = [];
 let currdis = 9;
 currdis +=10;
 output.textContent = currdis;
 //use to update the display while running functions?
 
+/*buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+         
+    })
+})*/
+
 numbers.forEach((numButton) => {
     numButton.addEventListener('click', () => {
+        workingVals.push(numButton.id);
         output.textContent = numButton.id;
        
     });
 });
 
 let opfunc;
+let total;
 //can turn this into a swtich statement
 operators.forEach((opButton) => {
     opButton.addEventListener('click', () => {
@@ -43,10 +52,12 @@ operators.forEach((opButton) => {
             opfunc = adds;
         } else if (opButton.id === 'subtract') {
             opfunc = subtracts;
-        } else if (opButon.id === 'multiply') {
+        } else if (opButton.id === 'multiply') {
             opfunc = multiply;
         } else if (opButton.id === 'divide') {
             opfunc = divide;
         } 
     })
 })
+
+
